b2875e1cc87b7f5cf2e93afdd384608bac1f084c
TaylerDB <bak12001@byui.edu>
2018-08-04 19:02:56 -0500

    Implemented a listView with buttons on the home page

---
+++ app/src/main/java/com/example/tayle/simplebudget/Budget.java
@@ -0,0 +1,5 @@
+package com.example.tayle.simplebudget;
+
+public class Budget {
+
+}
--- app/src/main/java/com/example/tayle/simplebudget/MainActivity.java
+++ app/src/main/java/com/example/tayle/simplebudget/MainActivity.java
@@ -9,67 +9,127 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /***************************************
  * Hours Spent on this project:
- * 2 hrs
+ * 7 hrs
 ****************************************/
 
 public class MainActivity extends AppCompatActivity {
 
-    //final Button addMoneyButton = (Button) findViewById(R.id.addButton);
-    //final Button takeMoneyButton = (Button) findViewById(R.id.takeButton);
+    // An adapter to interact between the _contactList and the contactListVeiw.
+    //BudgetsAdapter budgetsAdapter;
+
+    // All budgets are stored on this list
+    List<Budget> budgetList;
+
+    // Selected budget
+    private int selectedBudgetPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
+        populateListView();
+        registerClickCallBack();
+
+        // This makes it so the keyboard doesn't push up the buttons
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         // Wire up the button to do stuff
         // ..get the button
-        Button addMoneyButton = (Button) findViewById(R.id.addButton);
+        Button addNewBudgetButton = (Button) findViewById(R.id.addNewBudgetButton);
         // ..set what happens when the user clicks
-        addMoneyButton.setOnClickListener(new View.OnClickListener() {
+        addNewBudgetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("My App", "Add Money button was clicked");
-                Toast.makeText(getApplicationContext(), "Added Money", Toast.LENGTH_SHORT).show();
+                Log.i("My App", "Add new budget button was clicked");
+                Toast.makeText(getApplicationContext(), "Added New Budget", Toast.LENGTH_SHORT).show();
             }
         });
 
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //               .setAction("Action", null).show();
-        //    }
-        //});
+//        Button removeBudgetButton = (Button) findViewById(R.id.removeBudgetButton);
+//        // ..set what happens when the user clicks
+//        removeBudgetButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.i("My App", "remove budget button was clicked");
+//                Toast.makeText(getApplicationContext(), "Revoved Budget", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void populateListView() {
+
+        // Create list of items
+        String[] myItems = {"Blue", "Green", "Red", "Purple"};
+
+        // Build Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,                // Context for Activity
+                R.layout.item_budget,       // Layout to use (create)
+                myItems);                   // Items to be displayed
+
+        // Configure the list view
+        ListView list = (ListView) findViewById(R.id.budgetListView);
+        list.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void registerClickCallBack() {
+        ListView list = (ListView) findViewById(R.id.budgetListView);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View viewClicked, int position, long id) {
+                TextView textView = (TextView) viewClicked;
+                String message = "You clicked # " + position + ", which is string: " + textView.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onAddBudget(View btnAddBudget) {
+        Toast.makeText(getApplicationContext(), "Added New Budget", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRemoveBudget(View btnRemoveBudget) {
+        if (!budgetList.isEmpty()) {
+            // Make sure we don't try to delete at a negative index
+            if(selectedBudgetPosition < 0) {
+                selectedBudgetPosition = budgetList.size() - 1;
+            }
+            if (selectedBudgetPosition <= budgetList.size()) {
+                budgetList.remove(budgetList.remove(selectedBudgetPosition));
+                //budgetsAdapter.notifyDataSetChanged();
+//                if (budgetList.isEmpty()) {
+//                    _variablesList.clear();
+//                    _variablesAdapter.notifyDataSetChanged();
+//                }
+            }
+
+            // Check if the selected contact position is past the size of the list
+            if (selectedBudgetPosition >= budgetList.size()) {
+                // Make sure we don't set the position negative
+                selectedBudgetPosition--;
+//                _variablesList.clear();
+//                _variablesAdapter.notifyDataSetChanged();
+
+                // A hack for unselecting the empty space where the deleted contact was
+                ListView budgetListView = (ListView) findViewById(R.id.budgetListView);
+                budgetListView.setSelector(android.R.color.transparent);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
