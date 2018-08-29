d2ef9c78f22f672c89e2b5a566a638d068bb9aa8
TaylerDB <bak12001@byui.edu>
2018-08-28 21:09:56 -0500

    Buttons to add and remove budget

--- app/src/main/java/com/example/tayle/simplebudget/MainActivity.java
+++ app/src/main/java/com/example/tayle/simplebudget/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -17,10 +18,12 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -28,12 +31,18 @@ import java.util.Set;
 
 /***************************************
  * Hours Spent on this project:
- * 7 hrs
+ * 10 hrs
 ****************************************/
-
 public class MainActivity extends AppCompatActivity {
 
+    Button addNewBudgetButton;
+    Button removeNewBudgetButton;
+    TextView totalAmountView;
 
+    double budget = 0.00d;
+
+    // Sets decimal precision to two
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,35 +51,91 @@ public class MainActivity extends AppCompatActivity {
 
         // This makes it so the keyboard doesn't push up the buttons
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    } // End of onCreate()
 
-        // Wire up the button to do stuff
-        // ..get the button
-        Button addNewBudgetButton = (Button) findViewById(R.id.addNewBudgetButton);
-        // ..set what happens when the user clicks
-        addNewBudgetButton.setOnClickListener(new View.OnClickListener() {
+    /**
+     * This method will add a value to the budget
+     *      Using modified code from https://stackoverflow.com/questions/10903754/input-text-dialog-android
+     */
+    public void onAddValue(View view){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Value to be added to budget");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Log.i("My App", "Add new budget button was clicked");
+            public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(getApplicationContext(), "Added New Budget", Toast.LENGTH_SHORT).show();
+
+                // Convert string to a number
+                double newAddedValue = Double.parseDouble(input.getText().toString());
+                budget = budget + newAddedValue;
+                display(budget);
             }
         });
 
-        // Wire up the button to do stuff
-        // ..get the button
-        Button removeBudgetButton = (Button) findViewById(R.id.removeBudgetButton);
-        // ..set what happens when the user clicks
-        addNewBudgetButton.setOnClickListener(new View.OnClickListener() {
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Log.i("My App", "Remove new budget button was clicked");
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    } // End of onAddValue()
+
+    /**
+     * This method wil remove from the budget
+     *      Using modified code from https://stackoverflow.com/questions/10903754/input-text-dialog-android
+     */
+    public void onRemoveValue(View view){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Value to be removed from the budget");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("REMOVE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
                 Toast.makeText(getApplicationContext(), "Added New Budget", Toast.LENGTH_SHORT).show();
+
+                // Convert string to a number
+                double newRemovedValue = Double.parseDouble(input.getText().toString());
+                budget = budget - newRemovedValue;
+                display(budget);
             }
         });
 
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    } // End of onRemoveValue()
 
-
-    }
-
-
-
+    /**
+     * This method displays the budget
+     * @param number
+     */
+    private void display(double number){
+        // Find the totalAmount TextView
+        totalAmountView = (TextView)findViewById(R.id.totalAmount);
+        totalAmountView.setText("$" + df2.format(number));
+    } // End of display()
 }
